b13a49083b22db90d620c83e630f9f927247ff8c
João Gabriel Motta <jgmotta2004@gmail.com>
2024-12-13 15:16:16 -0300

    Adicionado switch/case para identificar o valor e qual tipo de moeda foi enviado pelo submit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+const USD = 6.02
+const GBP = 7.6
+const EUR = 6.32
+
 /* Obtendo os elementos do formulário */
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
@@ -12,5 +16,21 @@ amount.addEventListener("input", function (event) {
 /* Capturando o evento de submit do forumlário */
 form.onsubmit = function (event) {
   event.preventDefault()
-  console.log(currency.value)
+
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+      break
+  }
+}
+
+/* Função para converter a moeda */
+function convertCurrency(amount, price, symbol) {
+  console.log(amount, price, symbol)
 }
