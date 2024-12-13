492c30f872404d0a476d6b5253fbd3da3cde5259
João Gabriel Motta <jgmotta2004@gmail.com>
2024-12-13 14:43:37 -0300

    Adicionado consts form e currency, comentários e função para capturar o evento de submit do formulário. Scripts

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
+/* Obtendo os elementos do formulário */
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
-amount.addEventListener("input", (event) => {
+/* Manipulando o input amount para receber apenas números */
+amount.addEventListener("input", function (event) {
   const hasCharactersRegex = /\D+/g
   amount.value = amount.value.replace(hasCharactersRegex, "")
 })
+
+/* Capturando o evento de submit do forumlário */
+form.onsubmit = function (event) {
+  event.preventDefault()
+  console.log(currency.value)
+}
