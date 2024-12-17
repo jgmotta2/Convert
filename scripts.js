b64a23dc2d8a7a09aa794a09f2000796852e0e9b
João Gabriel Motta <jgmotta2004@gmail.com>
2024-12-17 14:02:55 -0300

    Adicionado função para formatar a moeda em real brasileiro

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ const footer = document.querySelector("footer")
 const description = document.getElementById("description")
 
 /* Manipulando o input amount para receber apenas números */
-amount.addEventListener("input", function (event) {
+amount.addEventListener("input", function () {
   const hasCharactersRegex = /\D+/g
   amount.value = amount.value.replace(hasCharactersRegex, "")
 })
@@ -35,11 +35,15 @@ form.onsubmit = function (event) {
 /* Função para converter a moeda */
 function convertCurrency(amount, price, symbol) {
   try {
+    /* exibindo a cotação da moeda selecionada. 
     if (currency.value == "USD" || currency.value == "GBP") {
       description.textContent = `${symbol} 1 = ${price}`
     } else {
       description.textContent = ` 1${symbol} = ${price}`
-    }
+    }*/
+    currency.value == "USD" || currency.value == "GBP"
+      ? (description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`)
+      : (description.textContent = `1${symbol} = ${formatCurrencyBRL(price)}`)
 
     /* Aplica a classe que exibe o footer para mostrar o resultado */
     footer.classList.add("show-result")
@@ -48,4 +52,13 @@ function convertCurrency(amount, price, symbol) {
     footer.classList.remove("show-result")
     alert("Algo deu errado, tente novamente!")
   }
+}
+
+/* formata a moeda em real brasileiro */
+function formatCurrencyBRL(value) {
+  /* o "Number" converte pra número para utilizar o toLocaleString para formatar no padrão BRL.*/
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
 }
